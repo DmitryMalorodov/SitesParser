e1df8347ad4852198e30fad51cb76f7085dd6680
dmalorodov <malorodovd@yandex.ru>
2024-11-21 17:43:33 +0300

    добавление парсинга текущей разницы курса валют в количестве и процентах

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,10 +9,13 @@ public class Main {
         List<String> bids = locators.getBids();
         List<String> asks = locators.getAsks();
         List<String> exchangeNames = locators.getExchangeNames();
+        List<String> differences = locators.getDifferences();
+        List<String> percents = locators.getPercents();
 
         for (int i = 0; i < bids.size(); i++) {
             System.out.println(exchangeNames.get(i));
             System.out.println("Bid = " + bids.get(i) + ", Ask = " + asks.get(i));
+            System.out.println("Dif = " + differences.get(i) + ", Per = " + percents.get(i));
         }
     }
 }
